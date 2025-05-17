94f4f8e9eb2ab21f551e9fea92ab5948d5ca3cd7
bimawp <bimawirya90@gmail.com>
2025-05-17 18:12:50 +0700

    hapus

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css'; // Pastikan file CSS diimpor di sini
 
-//import { defineConfig } from 'vite';//
-//import react from '@vitejs/plugin-react';//
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
