d2a40d9d4070f037862d73be01b0e5dc5c369bff
bimawp <bimawirya90@gmail.com>
2025-05-21 15:44:46 +0700

    update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,12 +42,14 @@ export default function Hero() {
               src="/profil1.webp"
               alt="Foto profil Bima Wiryadi Praja"
               loading="lazy"
-              initial={{ y: 40, opacity: 0 }}
-              animate={{ y: 0, opacity: 0.8 }}
+              initial={{ x: -60, opacity: 0 }}
+              animate={{ x: 0, opacity: 0.6 }}
               transition={{ duration: 1 }}
-              className="block md:absolute md:block md:left-0 md:top-0 w-full md:w-1/2 h-auto max-h-[300px] md:h-full object-cover object-center grayscale brightness-100 z-10 mt-6 md:mt-0 rounded-xl"
+              className="block md:absolute md:block md:left-0 md:top-0 w-full md:w-1/2 h-auto md:h-full object-cover object-center grayscale brightness-100 z-10 mt-6 md:mt-0"
+              style={{
+                mixBlendMode: "lighten",
+              }}
             />
-
         </picture>
 
         {/* Overlay hitam transparan seluruh background */}
