494a6c84434ecb92844fbf1ed198f38b5e612d5f
bimawp <bimawirya90@gmail.com>
2025-05-21 20:00:27 +0700

    update

--- src/components/document.jsx
+++ src/components/document.jsx
@@ -1,15 +1,15 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="id">
+    <Html>
       <Head>
-        {/* Google AdSense */}
+        {/* Tambahkan di sini */}
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6652570143802609"
           crossOrigin="anonymous"
-        />
+        ></script>
       </Head>
       <body>
         <Main />
